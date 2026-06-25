fab25061c03bf98816ab2e3577bbe01f1a878edd
BusinessFamily05 <louis.elbaz@icloud.com>
2026-06-25 13:41:48 +0300

    fix

---
+++ pwa/app/downloads/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function DownloadsPage() {
+  return (
+    <main>
+      <h1>Téléchargements</h1>
+      <p>Cette page est en cours de migration.</p>
+    </main>
+  )
+}
